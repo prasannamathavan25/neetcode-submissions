c6601de5e188dc9011dc45ad69821f91868fbc20
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-06-02 16:31:49 +0530

    Add: majority-element - submission-5

---
+++ Data Structures & Algorithms/majority-element/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        book = dict()
+        max_count = 0 
+        ind = 0 
+
+        for loop in range(len(nums)):
+            book[nums[loop]] = 1 + book.get(nums[loop] , 0 )
+            if book[nums[loop]] > len(nums) // 2 : 
+                return nums[loop]
+            
+            if book[nums[loop]] > max_count : 
+                max_count = max_count + 1
+                ind = loop
+
+        return nums[ind]
+        
+        
+        
