8c92a780a5198e82211e980b0304faebb5d52560
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-06-04 11:53:50 +0530

    Add: subarray-sum-equals-k - submission-4

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        ans = 0 
+        book = {0:1}
+        cur_sum = 0 
+
+        for n in nums : 
+            cur_sum += n 
+            diff = cur_sum - k 
+            ans += book.get(diff,0)
+
+            book[cur_sum] = 1 + book.get(cur_sum,0)
+        
+        return ans
+            
+
+        
