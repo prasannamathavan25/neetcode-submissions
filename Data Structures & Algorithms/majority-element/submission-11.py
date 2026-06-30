205d0f3160e6a0afec89a35d4d62fd0e036db052
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-06-30 12:44:20 +0530

    Add: majority-element - submission-11

---
+++ Data Structures & Algorithms/majority-element/submission-11.py
@@ -0,0 +1,8 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+        
+        nums.sort()
+        ind = (len(nums)) // 2 
+        print(ind)
+        return nums[ind]
