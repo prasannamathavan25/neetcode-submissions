2f0fee67ce8bae5be49e2e52a86f45e25e97fbd9
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-07-22 18:54:05 +0530

    Add: baseball-game - submission-9

---
+++ Data Structures & Algorithms/baseball-game/submission-9.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for op in operations : 
+            if op == '+':
+                stack.append(stack[-1] + stack[-2])
+                continue
+            if op == 'D':
+                stack.append(stack[-1] * 2)
+                continue 
+            if op == 'C':
+                stack.pop()
+                continue
+            stack.append(int(op))
+        ans = 0 
+        for num in stack:
+            ans = ans + num 
+        return ans
+        
+
+        
+
+
+            
+            
+        
