4123ffea12b3d515446d850a3e53b81692d99df4
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-08-26 11:14:11 +0530

    Add: remove-element - submission-17

---
+++ Data Structures & Algorithms/remove-element/submission-17.py
@@ -0,0 +1,25 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        lp =0 
+        rp = len(nums) - 1 
+
+
+
+        while lp <= rp :
+            if nums[lp] != val:
+                lp = lp + 1
+                continue
+            if nums[rp] == val:
+                rp = rp - 1
+                continue
+            nums[lp] = nums[rp]
+            rp = rp -1
+            lp = lp + 1
+        
+        return rp + 1
+
+            
+            
+            
+
+
