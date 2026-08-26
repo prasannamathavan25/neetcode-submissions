4a9f2a5ddad30ff64a60ae09a0fe6150371d1eb7
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-08-26 11:37:42 +0530

    Add: majority-element - submission-29

---
+++ Data Structures & Algorithms/majority-element/submission-29.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        book = defaultdict(int)
+
+        for item in nums:
+            book[item] = book[item] + 1
+            if book[item] > len(nums) // 2:
+                print(item)
+                return item
+            
+            
+
+            
