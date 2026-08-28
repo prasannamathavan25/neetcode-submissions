bc1ce3ceaea1ff28cfdc0cd4147678d527fc33a8
prasannamathavan25 <prasannamathavanj@gmail.com>
2026-08-28 09:16:21 +0530

    Add: majority-element-ii - submission-14

---
+++ Data Structures & Algorithms/majority-element-ii/submission-14.py
@@ -0,0 +1,16 @@
+from collections import Counter
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        book = Counter(nums)
+        ans = []
+        for item in book:
+            if book[item] > n //3 :
+                ans.append(item)
+           
+        return ans
+
+
+       
+        
+        
